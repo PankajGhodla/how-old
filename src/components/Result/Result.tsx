a74b8b4faf6156128d6b1ec55612a6f2fcb9014f
Pankaj <pgho580@aucklanduni.ac.nz>
2019-07-06 01:17:24 +1200

    All the text is black now.

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -13,7 +13,7 @@ export default class Result extends React.Component<IProps,{}> {
       <div className="dank">
         {
           this.props.result === "" && this.props.filelength>0 ?
-            <Loader type="TailSpin" color="#00BFFF" height={80} width={80} /> :
+            <Loader type="TailSpin" color="black" height={80} width={80} /> :
             <p>{this.props.result}</p>
         }
       </div>
